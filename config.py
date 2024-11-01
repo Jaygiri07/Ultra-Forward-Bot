9c1e71e57c3fca69259ab7ffc416e13c76710a25
Rebelstar <145977706+Jaygiri07@users.noreply.github.com>
2024-11-01 23:30:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "11611160")
+    API_HASH = os.environ.get("API_HASH", "f77d166da7542024c08b457a8fb525c4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7810389125:AAF7_8BxIjh2x3IDRfwmKPTH8y8hPwag3V0") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://rebelbotz22:vNcEEoNvSQ33d44K@cluster0.oj1hu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "rebelbotz")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6717382350').split()]
 
 
 class temp(object): 
